c78db87320b6c5e549f96dbfe57f3ef99a310aae
minhhuy2409 <164725462+minhhuy2409@users.noreply.github.com>
2024-11-17 09:02:16 +0700

    Add files via upload

---
+++ final_hp3/final_hp3/VCGuide.swift
@@ -0,0 +1,29 @@
+//
+//  VCGuide.swift
+//  final_hp3
+//
+//  Created by huy bin on 4/11/24.
+//
+
+import UIKit
+
+class VCGuide: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ final_hp3/final_hp3/VCMain.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  final_hp3
+//
+//  Created by huy bin on 4/11/24.
+//
+
+import UIKit
+
+class VCMain: UIViewController {
+    @IBOutlet weak var lblWelcome: UILabel!
+    
+    @IBOutlet weak var btnSecret: UIButton!
+    @IBOutlet weak var btnGuide: UIButton!
+    @IBAction func unwindToVCMain(segue: UIStoryboardSegue) {
+        
+    }
+    @IBAction func btnGuide(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
